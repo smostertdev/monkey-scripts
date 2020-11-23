4d6bdb8644f028b98025b5ba8eb902d9870a6848
Sylvan Mostert <smostert.dev@gmail.com>
2020-11-22 21:07:09 -0500

    Print header, fix organizatino

--- aliexpress.js
+++ aliexpress.js
@@ -26,25 +26,19 @@ $(".order-item-wraper").each((ind, el)=>{
     var hasTracking = $(el).find(".button-logisticsTracking ").length > 0;
     inum = 0;
 
-    // Retrieve each item
+    // Retrieve each product
     $(el).find(".order-body").each((i,e)=>{
         $(e).find(".product-sets").each((i,e)=>{
-            let item = {
-                item_name: $(e).find(".product-title").text().trim(),
-                item_url: $(e).find(".product-title .baobei-name").attr('href'),
-		        item_snapshot: $(e).find(".product-snapshot .baobei-name").attr('href'),
-                item_price: $(e).find(".product-amount span:first()").text().trim(), // remove parcer for different currency
-                item_amount: $(e).find(".product-amount span:eq(1)").text().trim().slice(1),
-                item_num: ++inum,
-                order_id: $(el).find(".order-info .first-row .info-body ").text().trim(),
-                order_status: $(el).find(".order-status .f-left").text().trim(),
-                order_price: $(el).find(".amount-num").text().trim(),
-                order_date: $(el).find(".order-info .second-row .info-body").text().trim(),
-                seller_name: $(el).find(".store-info .first-row .info-body").text().trim(),
-                hasTracking: hasTracking,
+            let product = {
+                product_name: $(e).find(".product-title").text().trim(),
+                product_url: $(e).find(".product-title .baobei-name").attr('href'),
+		        product_snapshot: $(e).find(".product-snapshot .baobei-name").attr('href'),
+                product_price: $(e).find(".product-amount span:first()").text().trim(), // remove parcer for different currency
+                product_quantity: $(e).find(".product-amount span:eq(1)").text().trim().slice(1),
+                product_num: ++inum,
             };
-            products.push(item);
-            //console.log(item);
+            products.push(product);
+            //console.log(product);
         });
         //console.log(products);
     });
@@ -53,8 +47,6 @@ $(".order-item-wraper").each((ind, el)=>{
         id: $(el).find(".order-info .first-row .info-body ").text().trim(),
         status: $(el).find(".order-status .f-left").text().trim(),
         orderPrice: $(el).find(".amount-num").text().trim(),
-        productPriceAndAmount: $(el).find(".product-right .product-amount").text().trim().replace(/(?:\s\s)/g, ""),
-        productsNames: products.map((it)=> it.title).join(", "),
 	    orderDate: $(el).find(".order-info .second-row .info-body").text().trim(),
 	    sellerName: $(el).find(".store-info .first-row .info-body").text().trim(),
         hasTracking: hasTracking,
@@ -64,6 +56,26 @@ $(".order-item-wraper").each((ind, el)=>{
     orders.push(order);
 });
 
+function printHeader() {
+    var header = "";
+
+    header += "Order ID\t";
+    header += "Order Status\t";
+    header += "Order Price\t";
+    header += "Order Date\t";
+    header += "Seller Name\t";
+    header += "Has Tracking\t";
+    header += "Product Name\t";
+    header += "Product URL\t";
+    header += "Pruduct Snapshot\t";
+    header += "Pruduct Price\t";
+    header += "Product Quantity\t";
+    header += "Product Number\t";
+
+    header += "\n";
+
+    return header;
+}
 
 $('<button/>', {
     text: "LOAD",
@@ -72,29 +84,23 @@ $('<button/>', {
         $("#csvBtn").text("Loading...");
         var s = "";
 
+        s += printHeader();
+
         orders.forEach(order=> {
 
             order.products.forEach(product => {
                 s += order.id + "\t";
                 s += order.status + "\t";
                 s += order.orderPrice + "\t";
-                s += order.productPriceAndAmount + "\t";
-                s += order.productsNames + "\t";
                 s += order.orderDate + "\t";
                 s += order.sellerName + "\t";
                 s += order.hasTracking + "\t";
-                s += product.item_name + "\t";
-                s += "https:" + product.item_url + "\t";
-                s += "https:" + product.item_snapshot + "\t";
-                s += product.item_price + "\t";
-                s += product.item_amount + "\t";
-                s += product.item_num + "\t";
-                s += product.order_id + "\t";
-                s += product.order_status + "\t";
-                s += product.order_price + "\t";
-                s += product.order_date + "\t";
-                s += product.seller_name + "\t";
-                s += product.hasTracking + "\t";
+                s += product.product_name + "\t";
+                s += "https:" + product.product_url + "\t";
+                s += "https:" + product.product_snapshot + "\t";
+                s += product.product_price + "\t";
+                s += product.product_quantity + "\t";
+                s += product.product_num + "\t";
                 s += "\n";
             })
         });
@@ -102,7 +108,6 @@ $('<button/>', {
         //console.log(s);
         GM_setClipboard (s);
         $("#csvBtn").text("Loaded to clipboard");
-
 
     }
 }).appendTo("#appeal-alert");
